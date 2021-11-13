e9fb1b5b75690f82c1f528a0b8900348c2ee8ef6
VENKATESH <VENKATESH@LAPTOP-P73SJJU0>
2021-11-13 11:34:14 +0530

    added my name

--- MyProject/Program.cs
+++ MyProject/Program.cs
@@ -7,6 +7,7 @@ namespace MyProject
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Console.WriteLine("Venkates babu");
         }
     }
 }
